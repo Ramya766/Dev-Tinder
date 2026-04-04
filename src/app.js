e22ac5b722d9cfba42663bd8e2298c798cb3522d
230701262 <ramyasreevarshini766@gmail.com>
2026-04-04 18:50:46 +0530

    routing

--- src/app.js
+++ src/app.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const app = express();
 //request handlers
-app.get("/user",(req,res)=>{
+app.get("/user/:userID/:name",(req,res)=>{
+  console.log(req.params);
   res.send("Hello user!!");
 });
-app.post("/user",(req,res)=>{
-  res.send("User Data saved to DB successfully!!");
-})
-app.delete("/user",(req,res)=>{
-  res.send("Data deleted successfully!!");
-})
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
