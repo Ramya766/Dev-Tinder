fcd64e2997a1c1ff68515821f1a81a81d34be953
230701262 <ramyasreevarshini766@gmail.com>
2026-04-05 21:47:50 +0530

    handling multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
-//request handlers
-app.get("/user/:userID/:name",(req,res)=>{
-  console.log(req.params);
-  res.send("Hello user!!");
-});
 
+app.use("/user",(req,res,next)=>{
+  const response="Ram"; //doubt
+  console.log("response1");
+  
+  next();
+  res.send(response);
+},(req,res)=>{
+  console.log("response2");
+  res.send("response2");
+ 
+})
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
