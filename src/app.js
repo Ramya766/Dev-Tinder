4b55e725637575feafdc7ee8bfbbb4bc197d61f9
230701262 <ramyasreevarshini766@gmail.com>
2026-04-08 21:26:02 +0530

    Middleware Example

--- src/app.js
+++ src/app.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const app = express();
+//authorize logic for all admin requests
+app.use("/admin", (req, res, next) => {
+  console.log("Auth called!!");
+  const token = "xyz123";
+  const auth = token === "xyz123";
+  if (!auth) {
+    res.status(401).send("Unauthorized user!");
+  } else {
+    next();
+  }
+});
+app.get("/admin/getData", (req, res) => {
+  res.send("All User Data");
+});
+app.get("/admin/delete", (req, res) => {
+  res.send("User Deleted!!");
+});
 
-app.use("/user",[(req,res,next)=>{
-  const response="Ram"; //doubt
-  console.log("response1");
-  
-  next();
-  res.send(response);
-},(req,res)=>{
-  console.log("response2");
-  res.send("response2");
- 
-}]);
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
-
-
