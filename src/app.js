ca9faea76d75b2b0d98f4ed25e540b08fe888e0c
230701262 <ramyasreevarshini766@gmail.com>
2026-04-10 11:25:56 +0530

    Connected to db using mongoose

--- src/app.js
+++ src/app.js
@@ -1,14 +1,6 @@
 const express = require("express");
+require("./Config/database");
 const app = express();
-app.get("/user", (req, res) => {
-  //throw new Error("djbjld;fkn ls"); //creating error for testing
-  res.send("User Data!!");
-});
-app.use("/", (err, req, res, next) => {
-  if (err) {
-    res.status(500).send("Internal Server Error: ");
-  }
-});
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
