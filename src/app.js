76d517359ab4473f423d8a4dbdebdad23d755b5d
230701262 <ramyasreevarshini766@gmail.com>
2026-04-16 23:32:06 +0530

    get API's

--- src/app.js
+++ src/app.js
@@ -3,6 +3,26 @@ const connectDB = require("./Config/database");
 const User = require("./models/user");
 const app = express();
 app.use(express.json());
+app.get("/user", async (req, res) => {
+  try {
+    const users = await User.findOne({ emailId: req.body.emailId });
+    if (!users) {
+      res.status(404).send("User not found!!");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went worng!!");
+  }
+});
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went worng!!");
+  }
+});
 app.post("/signup", async (req, res) => {
   const user = new User(req.body);
   try {
